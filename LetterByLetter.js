198c1172c18285609ea752374b267f057d124626
mariuselfman <marius.elfman@gmail.com>
2023-09-24 22:36:14 +0200

    first commit

---
+++ LetterByLetter.js
@@ -0,0 +1,11 @@
+function printLetterByLetter(destination, message, speed){
+    var i = 0;
+    var interval = setInterval(function(){
+        document.getElementById(destination).innerHTML += message.charAt(i);
+        i++;
+        if (i > message.length){
+            clearInterval(interval);
+        }
+    }, speed);
+}
+printLetterByLetter("element-id", "Lorem Ipsum", 10);
